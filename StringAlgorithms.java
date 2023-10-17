76a339ea1751256be980f15420e53c0eca0dbd7a
Ryan Baber <rbaber@episcopalacademy.org>
2023-10-17 14:29:26 -0400

    initial commit

---
+++ StringAlgorithms.java
@@ -0,0 +1,25 @@
+public class StringAlgorithms{
+    public static void main(String[] args) {
+
+    }
+  
+    /*
+     * Write the implementation for the following method
+     * that takes a String parameter and returns a copy of
+     * that String value in reverse order.
+     */
+    public static String reverse(String text) {
+      
+      return "";
+    }
+  
+    /*
+     * Wrie a method that takes two String parameters
+     * and returns an int representing the number of times
+     * the second String is present in the first.
+     */
+    public static int count(String text, String sub) {
+      
+      return 0;
+    }
+}
